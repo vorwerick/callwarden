63c2c19e350a958a1221e8425b384324af89bda6
vojjta <ota.sverma@gmail.com>
2023-04-28 15:33:47 +0200

    added repeating sync (#4)

--- app/src/main/java/cz/dzubera/callwarden/ui/activity/MainActivity.kt
+++ app/src/main/java/cz/dzubera/callwarden/ui/activity/MainActivity.kt
@@ -241,6 +241,10 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // start alarm
+        AlarmUtils.scheduleAlarm(applicationContext)
+
         val credentials = PreferencesUtils.loadCredentials(this)
 
         if (credentials == null) {
